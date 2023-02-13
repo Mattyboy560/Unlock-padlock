962c83c8aaee174c6a7cf89f349f0fb5518e1d39
MATT GREEN <107582947+Mattyboy560@users.noreply.github.com>
2023-02-13 19:54:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+img id="my image" src="lockedpadlock.jpg" height="150" width="150">
+  <br>
+
+
+  <button onclick ="document.getElementById('my image').src='unlockedpadlock.jpg'">Unlock me!</button>
